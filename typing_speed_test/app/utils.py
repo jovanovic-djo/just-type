2cf0cae8bb04245256eec6edb95b991b332d2e79
Djordje J <jovanovic.djo@gmail.com>
2024-06-14 22:49:31 +0200

    improve file handling and add more json word sets

--- typing_speed_test/app/utils.py
+++ typing_speed_test/app/utils.py
@@ -1,14 +1,29 @@
 import json
 import os
+import random
 
 
-def load_words(language, accent):
+def load_words(language, accent, topic, complexity, word_count):
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_dir, "words", f"{language}.json")
+
+        
+    if topic == "none":
+        file_path = os.path.join(current_dir, "words", f"{language}{complexity}.json")
+    elif topic == "numbers" or topic == "quotes":
+        file_path = os.path.join(current_dir, "words", f"{topic}.json")
+    else:
+        file_path = os.path.join(current_dir, "words", f"{language}{topic}.json")
+ 
 
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
         words = data.get("words", [])
+        if topic == "quotes":
+            random.shuffle(words)
+            words = words[:1]
+        else:
+            random.shuffle(words)
+            words = words[:word_count]
         if accent == "off":
             for word in words:
                 word.replace(
--- typing_speed_test/app/views.py
+++ typing_speed_test/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .utils import load_words
-import random
+
 
 
 def index(request):
@@ -11,11 +11,11 @@ def type(request):
     if request.method == "POST":
         language = request.POST.get("language")
         word_count = int(request.POST.get("words"))
+        accent = request.POST.get("accent")
+        topic = request.POST.get("topic")
+        complexity = request.POST.get("complexity")
 
-        words = load_words(language)
-        random.shuffle(words)
-
-        selected_words = words[:word_count]
-
-        return render(request, "typing_speed_test/type.html", {"words": selected_words})
+        words = load_words(language, accent, topic, complexity, word_count)
+        
+        return render(request, "typing_speed_test/type.html", {"words": words})
     return render(request, "typing_speed_test/type.html")
